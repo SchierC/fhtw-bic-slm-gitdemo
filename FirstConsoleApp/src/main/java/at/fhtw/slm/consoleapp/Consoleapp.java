08417aa1e2c800fab8d4c5b0404e62d618b4df4b
SchierC <schier.cosmin@gmail.com>
2023-10-04 19:12:30 +0200

    user changes

--- FirstConsoleApp/src/main/java/at/fhtw/slm/consoleapp/Consoleapp.java
+++ FirstConsoleApp/src/main/java/at/fhtw/slm/consoleapp/Consoleapp.java
@@ -3,5 +3,9 @@ package at.fhtw.slm.consoleapp;
 public class Consoleapp {
     public static void main(String[] args) {
         System.out.println("a big hello to ...");
+
+        User user = new User(1, "test", "test");
+        User user2 = new User(2, "test2", "test2");
+        
     }
 }
--- FirstConsoleApp/src/main/java/at/fhtw/slm/consoleapp/User.java
+++ FirstConsoleApp/src/main/java/at/fhtw/slm/consoleapp/User.java
@@ -5,6 +5,12 @@ public class User {
     private String login;
     private String password;
 
+    public User(int id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
     public String getPassword() {
         return password;
     }
